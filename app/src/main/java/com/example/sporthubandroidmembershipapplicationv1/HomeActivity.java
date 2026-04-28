41a1bfd862f97a50c04f0a15ec51e4c695af1f41
dhonburi <dhonlao2005@gmail.com>
2026-04-28 16:33:17 +1200

    Login Thingy

--- app/src/main/java/com/example/sporthubandroidmembershipapplicationv1/HomeActivity.java
+++ app/src/main/java/com/example/sporthubandroidmembershipapplicationv1/HomeActivity.java
@@ -1,6 +1,10 @@
 package com.example.sporthubandroidmembershipapplicationv1;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
+    String Username;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Intent i = getIntent();
+        Username = i.getStringExtra("Username");
+        View v = findViewById(R.id.textView);
+        String welcomeMessage = "Welcome " + Username + "!";
+        ((TextView)v).setText(welcomeMessage);
+    }
+
+    public void Logout(){
+        // Open the login page and clear session
+
+        // TEMPORARY - MAKE IT MORE SECURE
+        Username = "";
+
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
     }
 }
