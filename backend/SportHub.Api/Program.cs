69be6ec7a88628ed9975a592eb890b14ea37397c
dhonburi <dhonlao2005@gmail.com>
2026-07-27 15:32:45 +1200

    Create initial authentication database schema

--- backend/SportHub.Api/Program.cs
+++ backend/SportHub.Api/Program.cs
@@ -1,15 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using SportHub.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<SportHubDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString(
+            "SportHubDatabase"
+        )
+    )
+);
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
